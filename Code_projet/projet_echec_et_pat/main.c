8bb69c068153350d2425abca746d45e1bc7b6875
VincentGauweiler <vgauweiler@gmail.com>
2018-10-15 11:10:09 +0200

    graphique.h

--- Code_projet/projet_echec_et_pat/main.c
+++ Code_projet/projet_echec_et_pat/main.c
@@ -4,19 +4,17 @@ avec affichage sur le terminal*/
 
 /* Mettre tout les #include ici*/
 #include <stdio.h>
-#include "initJeu.h"
 
 /* Mettre tout les #define ici*/
 #define TAILLETERRAIN 8  // un carré de 8 sur 8
+
+#include "initJeu.h"
+#include "graphiqueJeu.h"
 
 
 /* Mettre les variables globales ici
 (si ce n'est pas possible de faire autrement)*/
 
-
-
-void affichageMenu();  // affichage du menu de départ
-void affichageJeu();  // affichage du terrain, des pions
 
 void calculsVictoire();  // à chaque tour, pour les deux roi (verifie si l'action est réalisable)
 void calculsMouvement();  // à chaque fois que l'on change la disposition des pieces
