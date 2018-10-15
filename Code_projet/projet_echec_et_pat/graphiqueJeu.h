6805294be584fbb5f20e276bbcd3c80a4bba92d2
VincentGauweiler <44134384+VincentGauweiler@users.noreply.github.com>
2018-10-15 11:17:45 +0200

    Graphique.h (#40)
    * Premier.h
    
    Je suis heureux!
    
    * Revert "Premier.h"
    
    This reverts commit b5d4ffdb45884ec6f07a2e475a93fae4fd012426.
    
    * Revert "Revert "Premier.h""
    
    This reverts commit 8aa683151832f51b94c8194e541221f2d8127fb0.
    
    * synchro (#37)
    
    * Delete echec_et_pat.c
    
    * Delete echec_et_pat
    
    * iniJeu (#35)
    
    * Premier.h
    
    Je suis heureux!
    
    * Revert "Premier.h"
    
    This reverts commit b5d4ffdb45884ec6f07a2e475a93fae4fd012426.
    
    * Revert "Revert "Premier.h""
    
    This reverts commit 8aa683151832f51b94c8194e541221f2d8127fb0.
    
    * graphique.h
    
    * Rassemblement general

---
+++ Code_projet/projet_echec_et_pat/graphiqueJeu.h
@@ -0,0 +1,8 @@
+#ifndef GRAPHIQUEJEU_H_INCLUDED
+#define GRAPHIQUEJEU_H_INCLUDED
+
+
+void affichageMenu();  // affichage du menu de départ
+void affichageJeu();  // affichage du terrain, des pions
+
+#endif // GRAPHIQUEJEU_H_INCLUDED
--- Code_projet/projet_echec_et_pat/main.c
+++ Code_projet/projet_echec_et_pat/main.c
@@ -1,25 +1,23 @@
 /* Debut de ce "fantastique" projet pour faire un jeu d'echec
 avec affichage sur le terminal*/
 
+/* Mettre tout les #define ici*/
+#define TAILLETERRAIN 8  // un carrÃ© de 8 sur 8
+
 
 /* Mettre tout les #include ici*/
 #include <stdio.h>
-#include "initJeu.h"
 
-/* Mettre tout les #define ici*/
-#define TAILLETERRAIN 8  // un carré de 8 sur 8
+#include "initJeu.h"
+#include "graphiqueJeu.h"
 
 
 /* Mettre les variables globales ici
 (si ce n'est pas possible de faire autrement)*/
 
 
-
-void affichageMenu();  // affichage du menu de départ
-void affichageJeu();  // affichage du terrain, des pions
-
-void calculsVictoire();  // à chaque tour, pour les deux roi (verifie si l'action est réalisable)
-void calculsMouvement();  // à chaque fois que l'on change la disposition des pieces
+void calculsVictoire();  // Ã  chaque tour, pour les deux roi (verifie si l'action est rÃ©alisable)
+void calculsMouvement();  // Ã  chaque fois que l'on change la disposition des pieces
 
 
 /* Fonction main*/
