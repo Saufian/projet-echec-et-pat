14519bb4c6731c01dd41013a9cace5fc1512aefc
Thomas <32197496+Saufian@users.noreply.github.com>
2018-10-18 21:24:25 +0200

    Mise en place des pieces et des fonctions sur les listes de pieces
    * Delete gestionListe.c
    
    * Delete initJeu.c
    
    * Delete initJeu.h
    
    * Delete main.c
    
    * Delete projet_echec_et_pat.cbp
    
    * Delete projet_echec_et_pat.depend
    
    * Placement des pieces
    
    Et mise en place des fonctions pour gérer les listes de pieces des joueurs
    
    * Mise à jours pour la fonction eraseListePiece
    
    * Mise à jour pour eraseListePiece
    
    * Modification de l'encodage des caracteres
    
    On change les é, è, et à
    
    * suite de la correction

--- Code_projet/projet_echec_et_pat/initJeu.h
+++ Code_projet/projet_echec_et_pat/initJeu.h
@@ -8,7 +8,7 @@
 
 
 /* Mettre tout les #define ici*/
-#define TAILLETERRAIN 8  // un carré de 8 sur 8
+#define TAILLETERRAIN 8  // un carrÃ© de 8 sur 8
 #define NMBPIECEPARJOUEUR 16  // nombre initial de piece pour chaque joueur
 
 
@@ -18,9 +18,9 @@
 
 /* Mettre les declaration des structures ici*/
 
-typedef struct Element {  // structure pour réaliser une liste contenant les cases qui pourront etre atteintes
-    int posX;  // coordonnées en x  (de 1 a 8 inclus)
-    int posY;  // coordonnées en y
+typedef struct Element {  // structure pour realiser une liste contenant les cases qui pourront etre atteintes
+    int posX;  // coordonnees en x  (de 1 a 8 inclus)
+    int posY;  // coordonnees en y
     struct Element* precedent;  // contient l'adresse de l'element precedent
     struct Element* suivant;  // contient l'adresse de l'element suivant
 } Element;
@@ -44,7 +44,7 @@ typedef struct Piece {
     int posX;  // toujours utile
     int posY;
     Joueur* possesseur;  // determine la couleur de la piece, et qui peut la bouger
-    Element* mouvementPossible;  // on stocke les differents endroits où la piece peut aller
+    Element* mouvementPossible;  // on stocke les differents endroits oÃ¹ la piece peut aller
 } Piece;
 
 typedef struct Case {
@@ -62,30 +62,30 @@ typedef struct Case {
 
 // partie pour l'initialisation du terrain
 void initJeu(Case terrain[TAILLETERRAIN][TAILLETERRAIN], Joueur* joueur1, Joueur* joueur2);
-// met en place les éléments du jeu (terrain, pieces, liste de pieces, etc)
+// met en place les elements du jeu (terrain, pieces, liste de pieces, etc)
 void initTerrain(Case terrain[TAILLETERRAIN][TAILLETERRAIN]);
 // rempli le terrain de cases blanches et noires
 Joueur* initJoueur(char* nom, int couleur);
 // rempli les infos relatives aux joueurs
 Case initCase(int couleur, int posX, int posY);  // initialisation des cases (on met le contenu NULL)
 Piece* initPiece(char type, int posX, int posY, Joueur* possesseur);
-// créé les pièces (type, couleur, position de base), puis utilise calculsMouvement
+// cree les pieces (type, couleur, position de base), puis utilise calculsMouvement
 void initListePiece(int taille, Piece* liste[taille]);  // pour initialiser les listes de pieces
 void addListePiece(int taille, Piece* liste[taille],Piece* elementSupplementaire);  // on ajoute en fin de liste
-void eraseListePiece(int taille, Piece* liste[taille], int numElement);  // suppression de l'element
+void eraseListePiece(int taille, Piece* liste[taille], Piece* cible);  // suppression de l'element
 
 // partie affichage
-void affichageMenu();  // affichage du menu de départ
+void affichageMenu();  // affichage du menu de depart
 void affichageJeu(); // affichage du terrain, des pions
 
 // partie calcul
-void calculsVictoire();  // à chaque tour, pour les deux roi (verifie si l'action est réalisable)
-void calculsMouvement();  // à chaque fois que l'on change la disposition des pieces
+void calculsVictoire();  // aÂ chaque tour, pour les deux roi (verifie si l'action est realisable)
+void calculsMouvement();  // a chaque fois que l'on change la disposition des pieces
 
 //partie gestion de liste
 Element* initElement(int posX, int posY);  // Initialisation des elements
 Element* addListe(Element* elementListe, Element* elementSupplementaire);  // ajoute un element a une liste
-Element* eraseListe(Element* liste);  // supprimer une liste, et liberer l'espace memoire alloué
+Element* eraseListe(Element* liste);  // supprimer une liste, et liberer l'espace memoire allouÃ©
 void afficheListe(Element* liste);  // pour visualiser les listes (debug)
 
 #endif // INITJEU_H_INCLUDED
