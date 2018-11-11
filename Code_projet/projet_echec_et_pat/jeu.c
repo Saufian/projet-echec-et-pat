3f577b3c5a445e15cd9f2244d89af7ce2726e5de
Thomas <32197496+Saufian@users.noreply.github.com>
2018-11-11 23:24:43 +0100

    Update jeu.c

--- Code_projet/projet_echec_et_pat/jeu.c
+++ Code_projet/projet_echec_et_pat/jeu.c
@@ -7,22 +7,73 @@ Joueur* partieDeuxJoueurs(Case terrain[TAILLETERRAIN][TAILLETERRAIN], Joueur* jo
     /* fonction principale pour le jeux a 2 joueurs*/
     Joueur* gagnant = NULL;
 
-    /* affichage experimental du terrain*/
-    for (int i = 0; i<TAILLETERRAIN; i++) {
-        for (int j = 0; j<TAILLETERRAIN; j++) {
-            if (terrain[j][TAILLETERRAIN -1 -i].contenu == NULL) {
-                if (terrain[j][TAILLETERRAIN -1 -i].couleur == 0) printf("   ");
-                else if (terrain[j][TAILLETERRAIN -1 -i].couleur == 1) printf(" - ");
-                else printf(" $ ");
+    affichageTerrain(terrain);
+
+    /* calcul des positions pour le joueur 1*/
+    calculMouvement(joueur1->listePiece, terrain);
+    gestionSurbrillance(terrain, joueur1->listePiece[4]->mouvementPossible);  // test, on affiche le deplacement potentiel du roi
+    calculMouvement(joueur2->listePiece, terrain);
+
+    affichageTerrain(terrain);
+    printf("affichage des deplacement possible du roi\n");
+    return gagnant;
+}
+
+
+void affichageTerrain(Case terrain[TAILLETERRAIN][TAILLETERRAIN]) {
+	/* affichage experimental du terrain
+	 * on met en place TAILLETERRAIN-1 - y pour que le point de coordonné 0,0 soit en bas a gauche
+	 */
+    for (int i = 0; i < 10; i++) {  // on fait un peu d'espace pour l'affichage
+        printf("\n");
+    }
+    /* affichage du terrain*/
+    for (int y = 0; y<TAILLETERRAIN; y++) {
+        for (int x = 0; x<TAILLETERRAIN; x++) {
+            if (terrain[x][TAILLETERRAIN-1 - y].contenu == NULL) {  // s'il n'y a pas de piece sur cette case
+            	if (terrain[x][TAILLETERRAIN-1 - y].surbrillance == 0) {  // dans le cas ou il n'y a pas de subrillance
+	                if (terrain[x][TAILLETERRAIN-1 - y].couleur == 0) printf("   ");  // case noire
+	                else if (terrain[x][TAILLETERRAIN-1 - y].couleur == 1) printf(" - ");  // case blanche
+	            }
+	            else printf(" * ");  // affichage de la surbrillance
             }
             else {
-                printf(" %c ", terrain[j][TAILLETERRAIN -1 -i].contenu->type);
+                printf(" %c ", terrain[x][TAILLETERRAIN-1 - y].contenu->type);  // affiche le type de la piece
             }
         }
         printf("\n");
     }
+}
 
-    /* calcul des positions pour le joueur 1*/
-    calculMouvement(joueur1->listePiece, terrain);
-    return gagnant;
+
+void gestionSurbrillance(Case terrain[TAILLETERRAIN][TAILLETERRAIN], Element* liste){
+	/*Initialisation, on nettoie toutes les cases du terrain*/
+	for (int x = 0; x < TAILLETERRAIN; x++) {  // on parcours le terrain
+		for (int y = 0; y < TAILLETERRAIN; y++) {
+			if (terrain[x][y].surbrillance == 1) {
+				terrain[x][y].surbrillance = 0;  // si le terrain est en surbrillance, on efface ce comportement
+			}
+		}
+	}
+	if (liste != NULL) {  // On fait attention au cas ou la liste est vide
+		Element* curseur = liste;
+		while (curseur != NULL) {  // on parcours la liste
+			terrain[curseur->posX][curseur->posY].surbrillance = 1;  // on met la case choisie en surbrillance
+			curseur = curseur->suivant;
+		}
+	}
+}
+
+void scanDeuxJoueurs(Joueur joueurActuel, int commande[2][2]) {
+	/* initialisation des cases du tableau commandes */
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			commande[i][j] = 0;
+		}
+	}
+
+	/* Interraction 1 avec le joueur
+	 * Demande de la piece a bouger
+	 */
+
 }
