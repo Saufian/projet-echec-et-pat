aebdb420fb6583c291957783206f6338836e710e
Thomas <32197496+Saufian@users.noreply.github.com>
2018-11-13 23:56:53 +0100

    Ajout de la license

--- Code_projet/projet_echec_et_pat/jeu.c
+++ Code_projet/projet_echec_et_pat/jeu.c
@@ -1,11 +1,26 @@
-/* Jeu.c
+/* School project, creation of a chess game in C, and with the content in French, I'm sorry about that.
+ * Copyright (C), 2018,  Thomas Bailleux, Vincent Gauweiler, Antoine Ruiz, from EILCO France.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details. 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/* Jeu.compteur
 contient les fonctions pour animer une partie*/
 
 #include "initJeu.h"
 
 Joueur* partieDeuxJoueurs(Case terrain[TAILLETERRAIN][TAILLETERRAIN], Joueur* joueur1, Joueur* joueur2) {
-	//Rajouter un test si fichier existe partie charger pour le lancer en mode ajout récupérer les donnée et repositioner le pointeur dans le fichier
-	FILE * saveFile=initSaveGame(joueur1,joueur2);//On initialise le fichier de sauvgarde todo:lui donner un nom different par partie pour stocker différente partie
 	/* fonction principale pour le jeux a 2 joueurs*/
 	Joueur* gagnant = NULL;
 	Joueur* joueurActuel = joueur1;
@@ -16,25 +31,28 @@ Joueur* partieDeuxJoueurs(Case terrain[TAILLETERRAIN][TAILLETERRAIN], Joueur* jo
 
 	while(gagnant == NULL) {
 		scanDeuxJoueurs(terrain, joueurActuel, mouvement);
-		saveGame(saveFile,mouvement);//je sauvegarde le déplacement OldX OldY NewX NewY
 		printf("%d,%d,  %d,%d", mouvement[0][0], mouvement[1][0], mouvement[0][1], mouvement[1][1]);
-		//deplacementPiece(terrain, mouvement);
+		deplacementPiece(terrain, mouvement);
 		calculMouvement(joueur1->listePiece, terrain);  // on recalcul les mouvements des pieces pour
 		calculMouvement(joueur2->listePiece, terrain);  // que ce soit a jour pour le test de victoire
 		affichageTerrain(terrain);  // on affiche le terrain avec la modification de la position
 		//gagnant = testVictoire(terrain, joueur1, joueur2);
 		if (joueurActuel == joueur1) joueurActuel = joueur2;
 		else joueurActuel = joueur1;
-		closeSaveGame(saveFile); //gagnant ne fonctionnent pas encore je dois fermer le fichier plutôt
 	}
-	//closeSaveGame(saveFile);
 	return gagnant;
 }
 
 
 void affichageTerrain(Case terrain[TAILLETERRAIN][TAILLETERRAIN]) {
+	/* Initialisation */
 	int curseurY, curseurX, compteur;
 	compteur = TAILLETERRAIN*TAILLETERRAIN + TAILLETERRAIN;  // on met en place la variable qui sert a afficher le numero de la ligne
+	/* Vidage de l'ecran */
+	for(int i = 0; i < 40; i++) {
+        printf("\n");  // On affiche du blanc
+	}
+	/* Affichage */
 	printf("\n\n   A      B      C      D      E      F      G      H \n" );  // affi
 	for (curseurY = 0; curseurY < TAILLETERRAIN; curseurY++) {  //Parcours des lignes
 		printf(" _____  _____  _____  _____  _____  _____  _____  _____ \n" );  // affichage de la partie superieure des cases
@@ -156,9 +174,30 @@ void scanDeuxJoueurs(Case terrain[TAILLETERRAIN][TAILLETERRAIN], Joueur* joueurA
 	commande[1][1] = posY;  // on stock dans le tableau avant de le renvoyer
 }
 
-/* TODO
 
-void deplacementPiece(Case terrain[TAILLETERRAIN][TAILLETERRAIN], int mouvement[2][2]);
+void deplacementPiece(Case terrain[TAILLETERRAIN][TAILLETERRAIN], int mouvement[2][2]) {
+	/* Initialisation */
+	Case* caseDepart = &terrain[mouvement[0][0]][mouvement[1][0]];  // on veut modifier le contenu de la case
+	Case* caseArrive = &terrain[mouvement[0][1]][mouvement[1][1]];
+	Piece* pieceDepart = caseDepart->contenu;  // le mouvement existe, donc on est sur qu'il y a une piece sur cette case
+
+	/* Cas ou il y a une piece a l'arrivee, que l'on va devoir supprimer*/
+	if (caseArrive->contenu != NULL) {
+		Piece* pieceArrive = caseArrive->contenu;
+		caseArrive->contenu = NULL;  // on vide la case d'arrive
+		/* on va enlever la piece de la liste du joueur qui la controle*/
+		eraseListePiece(NMBPIECEPARJOUEUR, pieceArrive->possesseur->listePiece, pieceArrive);
+	}
+
+	/* La case d'arrivee est maintenant libre*/
+	pieceDepart->posX = mouvement[0][1];  // on met a jours les coordonnees de la piece
+	pieceDepart->posY = mouvement[1][1];
+	caseArrive->contenu = pieceDepart;  // on met la piece dans la case d'arrive
+	caseDepart->contenu = NULL;  // on l'enleve de la case de depart
+}
+
+
+/* TODO
 Joueur* testVictoire(Case terrain[TAILLETERRAIN][TAILLETERRAIN], Joueur* joueur1, Joueur* joueur2);
 
 */
