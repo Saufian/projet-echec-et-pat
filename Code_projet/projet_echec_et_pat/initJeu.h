8791338ec6cf7970fb0bbfaa6f922262a72b7813
Thomas <32197496+Saufian@users.noreply.github.com>
2018-11-13 23:56:22 +0100

    Ajout de la license

--- Code_projet/projet_echec_et_pat/initJeu.h
+++ Code_projet/projet_echec_et_pat/initJeu.h
@@ -1,3 +1,20 @@
+/* School project, creation of a chess game in C, and with the content in French, I'm sorry about that.
+ * Copyright (C), 2018,  Thomas Bailleux, Vincent Gauweiler, Antoine Ruiz, from EILCO France.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details. 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef INITJEU_H_INCLUDED
 #define INITJEU_H_INCLUDED
 
@@ -8,7 +25,7 @@
 
 
 /* Mettre tout les #define ici*/
-#define TAILLETERRAIN 8  // un carré de 8 sur 8
+#define TAILLETERRAIN 8  // un carrÃ© de 8 sur 8
 #define NMBPIECEPARJOUEUR 16  // nombre initial de piece pour chaque joueur
 
 
@@ -18,9 +35,9 @@
 
 /* Mettre les declaration des structures ici*/
 
-typedef struct Element {  // structure pour réaliser une liste contenant les cases qui pourront etre atteintes
-    int posX;  // coordonnées en x  (de 0 a 7 inclus)
-    int posY;  // coordonnées en y
+typedef struct Element {  // structure pour rÃ©aliser une liste contenant les cases qui pourront etre atteintes
+    int posX;  // coordonnÃ©es en x  (de 0 a 7 inclus)
+    int posY;  // coordonnÃ©es en y
     struct Element* precedent;  // contient l'adresse de l'element precedent
     struct Element* suivant;  // contient l'adresse de l'element suivant
 } Element;
@@ -36,15 +53,15 @@ typedef struct Joueur {
 typedef struct Piece {
     char type;  // type de la piece
     /* P -> pions
-        T -> tour
-        C -> cavalier
-        F -> fou
-        R -> roi
-        D -> dame*/
+     *T -> tour
+     *C -> cavalier
+     *F -> fou
+     *R -> roi
+     *D -> dame*/
     int posX;  // toujours utile
     int posY;
     Joueur* possesseur;  // determine la couleur de la piece, et qui peut la bouger
-    Element* mouvementPossible;  // on stocke les differents endroits où la piece peut aller
+    Element* mouvementPossible;  // on stocke les differents endroits oÃ¹ la piece peut aller
 } Piece;
 
 typedef struct Case {
@@ -63,27 +80,27 @@ typedef struct Case {
 
 /* partie pour l'initialisation du terrain */
 void initJeu(Case terrain[TAILLETERRAIN][TAILLETERRAIN], Joueur* joueur1, Joueur* joueur2);
-// met en place les éléments du jeu (terrain, pieces, liste de pieces, etc)
+// met en place les Ã©lÃ©ments du jeu (terrain, pieces, liste de pieces, etc)
 void initTerrain(Case terrain[TAILLETERRAIN][TAILLETERRAIN]);
 // rempli le terrain de cases blanches et noires
 Joueur* initJoueur(char* nom, int couleur);
 // rempli les infos relatives aux joueurs
 Case initCase(int couleur, int posX, int posY);  // initialisation des cases (on met le contenu NULL)
 Piece* initPiece(char type, int posX, int posY, Joueur* possesseur);
-// créé les pièces (type, couleur, position de base), puis utilise calculsMouvement
+// crÃ©Ã© les piÃ¨ces (type, couleur, position de base), puis utilise calculsMouvement
 void initListePiece(int taille, Piece* liste[taille]);  // pour initialiser les listes de pieces
 void addListePiece(int taille, Piece* liste[taille],Piece* elementSupplementaire);  // on ajoute en fin de liste
 void eraseListePiece(int taille, Piece* liste[taille], Piece* cible);  // suppression de l'element
 
 
 /* partie affichage */
-void affichageMenu();  // affichage du menu de départ
+void affichageMenu(Case terrain[TAILLETERRAIN][TAILLETERRAIN], Joueur* joueur1, Joueur* joueur2);  // affichage du menu de dÃ©part
 
 
 /* partie calcul */
-void calculsVictoire();  // à chaque tour, pour les deux roi (verifie si l'action est réalisable)
+void calculsVictoire();  // Ã  chaque tour, pour les deux roi (verifie si l'action est rÃ©alisable)
 void calculMouvement(Piece * listePieceJoueur[NMBPIECEPARJOUEUR], Case terrain[TAILLETERRAIN][TAILLETERRAIN]);
-// à chaque fois que l'on change la disposition des pieces
+// Ã  chaque fois que l'on change la disposition des pieces
 Element* calculMouvementPion(Piece * piece, Case terrain[TAILLETERRAIN][TAILLETERRAIN]);
 Element* calculMouvementTour(Piece * piece, Case terrain[TAILLETERRAIN][TAILLETERRAIN]);
 Element* calculMouvementCavalier(Piece * piece, Case terrain[TAILLETERRAIN][TAILLETERRAIN]);
@@ -97,14 +114,14 @@ Element* calculMouvementLineaire(Piece * piece, Case terrain[TAILLETERRAIN][TAIL
 /* partie gestion de liste */
 Element* initElement(int posX, int posY);  // Initialisation des elements
 Element* addListe(Element* elementListe, Element* elementSupplementaire);  // ajoute un ou plusieurs element a une liste
-Element* eraseListe(Element* liste);  // supprimer une liste, et liberer l'espace memoire alloué
+Element* eraseListe(Element* liste);  // supprimer une liste, et liberer l'espace memoire allouÃ©
 int trouveElement(Element* liste, int posX, int posY);  // renvoi 1 si les pos peuvent etre trouve dans la liste d'element, renvoi 0 sinon
 void afficheListe(Element* liste);  // pour visualiser les listes (debug)
 
 
 /* Partie jeu */
 Joueur* partieDeuxJoueurs(Case terrain[TAILLETERRAIN][TAILLETERRAIN], Joueur* joueur1, Joueur* joueur2);
-// fonction principale lançant le jeu pour deux joueurs, et renvoi le joueur gagnant
+// fonction principale lanÃ§ant le jeu pour deux joueurs, et renvoi le joueur gagnant
 void affichageTerrain(Case terrain[TAILLETERRAIN][TAILLETERRAIN]);  // affiche l'etat du terrain.
 void gestionSurbrillance(Case terrain[TAILLETERRAIN][TAILLETERRAIN], Element* liste);
 // on donne en argument la liste des positions voulue. Vide la surbrillance des autres cases
@@ -112,16 +129,11 @@ void scanDeuxJoueurs(Case terrain[TAILLETERRAIN][TAILLETERRAIN], Joueur* joueurA
 /* scan les entrees des participants, et modifie un tableau[2][2] contenant la position de la piece a bouger,
  * et la position a laquelle le joueur veut la mettre. La fonction verifie si le mouvement est possible
  * (en regardant les positions possibles de la piece choisi, affiche en surbrillance les choix possible (qui correspondent
- * aux positions possibles de la piece selectionné, puis renvoi les informations rentrees par l'utilisateur dans commande si le mouvement
+ * aux positions possibles de la piece selectionnÃ©, puis renvoi les informations rentrees par l'utilisateur dans commande si le mouvement
  * est possible)). */
 void deplacementPiece(Case terrain[TAILLETERRAIN][TAILLETERRAIN], int mouvement[2][2]);
 // change de position la piece marque par mouvement, et gere le cas de la prise d'une piece ennemis
 Joueur* testVictoire(Case terrain[TAILLETERRAIN][TAILLETERRAIN], Joueur* joueur1, Joueur* joueur2);
-// verifit l'etat du roi de chaque coté, et renvoi le gagnant le cas echeant (renvoi NULL sinon).
-
-//Fonction de gestion de sauvegarde
-FILE * initSaveGame(Joueur* joueur1, Joueur* joueur2);
-FILE * saveGame(FILE * saveFile, int commande[2][2]);
-void closeSaveGame(FILE * saveFile);
+// verifie l'etat du roi de chaque cotÃ©, et renvoi le gagnant le cas echeant (renvoi NULL sinon).
 
 #endif // INITJEU_H_INCLUDED
